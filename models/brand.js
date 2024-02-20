e19868079c60a9bc73a4ffc0bdebccef408e05e3
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-02-19 17:01:39 -0800

    Add Brand model

---
+++ models/brand.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BrandSchema = new Schema({
+    name: { type: String, required: true, maxLength: 100 },
+    country_of_origin: { type: String, required: true, maxLength: 50 },
+    year_established: { type: Date }
+});
+
+// Set the brand's page url
+BrandSchema.virtual("url").get(function() {
+    return `/catalog/brand/${this._id}`;
+});
